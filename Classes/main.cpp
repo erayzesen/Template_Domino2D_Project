b5511371e8b84f4b39bcf76d5fccbd42ae01d9d7
Eray Zesen <erayzesen@gmail.com>
2020-05-25 20:57:53 +0300

    working on game class

--- Classes/main.cpp
+++ Classes/main.cpp
@@ -1,46 +1,11 @@
-//
-//  main.cpp
-//  Template_Domino2D_Project
-//
 //  Created by Eray Zesen on 25.05.2020.
-//  Copyright © 2020 Eray Zesen. All rights reserved.
-//
+
 
 #include <iostream>
-#include "SDL2/SDL.h"
 #include <stdio.h>
+#include "Domino2D.h"
 
 int main(int argc, const char * argv[]) {
-    SDL_Window *window;                    // Declare a pointer
-
-    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
-
-    // Create an application window with the following settings:
-    window = SDL_CreateWindow(
-        "An SDL2 window",                  // window title
-        SDL_WINDOWPOS_UNDEFINED,           // initial x position
-        SDL_WINDOWPOS_UNDEFINED,           // initial y position
-        640,                               // width, in pixels
-        480,                               // height, in pixels
-        SDL_WINDOW_OPENGL                  // flags - see below
-    );
-
-
-    // Check that the window was successfully created
-    if (window == NULL) {
-        // In the case that the window could not be made...
-        printf("Could not create window: %s\n", SDL_GetError());
-        return 1;
-    }
-
-    // The window is open: could enter program loop here (see SDL_PollEvent())
-
-    SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
-
-    // Close and destroy the window
-    SDL_DestroyWindow(window);
-
-    // Clean up
-    SDL_Quit();
+    Game::create("SampleGame", 640, 480)->run();
     return 0;
 }
